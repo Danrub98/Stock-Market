d4a6ec535afabcc8c2e35f6d4af68c0440a28931
sofiacrf <sofia.rojasflo@gmail.com>
2021-08-20 19:57:49 -0500

    working frontend

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,13 @@
 // Lets start with Link basic header
 // We have to import react
-import React from "react";
+import React,  { useState }  from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-
-
+import Auth from '../../utils/auth';
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <header>
       <Navbar collapseOnSelect expand="lg" fixed="top" bg="dark" variant="dark" style={{ backgroundColor: "black", color: "white", paddingBottom: "1.5%", paddingTop: "1.5%"}}>
@@ -17,7 +17,13 @@ const Header = () => {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/profile">Profile</Nav.Link>
+              {Auth.loggedIn() ? (
+                <>
+            <Nav.Link href="/profile">Profile</Nav.Link>
+</> ) :
+(
+            <Nav.Link href="/profile"></Nav.Link>
+)}
               <NavDropdown title="Markets" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="/charts">Stocks</NavDropdown.Item>
                 <NavDropdown.Divider />
@@ -30,19 +36,23 @@ const Header = () => {
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
+            {Auth.loggedIn() ? (
+                <>
+                  <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+</> ) :
+(
             <Nav>
-              <Nav.Link href="/signup">Sign up</Nav.Link>
+              <Nav.Link id="sign" href="/signup">Sign up</Nav.Link>
               <Nav.Link eventKey={2} href="/login">
                 Log in
               </Nav.Link>
             </Nav>
+)}
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
     </header>
   );
 };
-
 // Export the header
 export default Header;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,17 @@
 // Lets start with Link basic header
 // We have to import react
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import ListGroup from "react-bootstrap/ListGroup";
 import { MDBCol, MDBIcon } from "mdbreact";
-
+import Auth from '../utils/auth';
 // Import images
 import Mountain from "../images/mountain.jpg";
-
-
-
-
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <main>
       <section className="home-main-section">
@@ -41,7 +38,6 @@ const Home = () => {
       </section>
       <section className="widgets" style={{ backgroundColor: "black" }}>
         <h2 className="text-center">NASDAQ STOCKS</h2>
-
         <Row xs={1} md={2} className="g-4">
           <Col>
             <Card bg={"dark"} text={"white"} className="mb-3">
@@ -52,16 +48,21 @@ const Home = () => {
                 <Card.Text>
                   <ListGroup>
                     <ListGroup.Item variant="info">Ticker: APPL</ListGroup.Item>
-                    <ListGroup.Item variant="success">Price: </ListGroup.Item>
+                    <ListGroup.Item variant="success">Price:  47.89 </ListGroup.Item>
                     <ListGroup.Item action href="/charts" variant="primary">
                       Chart
                     </ListGroup.Item>
                   </ListGroup>
                 </Card.Text>
-                <Button className="text-center">Buy</Button>
-              </Card.Body>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Button className="text-center">Buy</Button>
+                    <Button className="text-center">Sell</Button>
+                  </>) : (
+                  <p className="text-center">Login to buy stocks </p>
+                )}
+            </Card.Body>
             </Card>
-
             <Card bg={"dark"} text={"white"} className="mb-3">
               <Card.Body>
                 <Card.Title className="text-center">
@@ -70,16 +71,21 @@ const Home = () => {
                 <Card.Text>
                   <ListGroup>
                     <ListGroup.Item variant="info">Ticker: PYPL</ListGroup.Item>
-                    <ListGroup.Item variant="success">Price: </ListGroup.Item>
+                    <ListGroup.Item variant="success">Price: 10</ListGroup.Item>
                     <ListGroup.Item action href="/charts" variant="primary">
                       Chart
                     </ListGroup.Item>
                   </ListGroup>
                 </Card.Text>
-                <Button variant="primary">Buy</Button>
-              </Card.Body>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Button className="text-center">Buy</Button>
+                    <Button className="text-center">Sell</Button>
+                  </>) : (
+                  <p className="text-center">Login to buy stocks </p>
+                )}
+            </Card.Body>
             </Card>
-
             <Card bg={"dark"} text={"white"} className="mb-3">
               <Card.Body>
                 <Card.Title className="text-center">
@@ -88,19 +94,24 @@ const Home = () => {
                 <Card.Text>
                   <ListGroup>
                     <ListGroup.Item variant="info">Ticker: AMZN</ListGroup.Item>
-                    <ListGroup.Item variant="success">Price: </ListGroup.Item>
+                    <ListGroup.Item variant="success">Price: 40</ListGroup.Item>
                     <ListGroup.Item action href="/charts" variant="primary">
                       Chart
                     </ListGroup.Item>
                   </ListGroup>
                 </Card.Text>
-                <Button variant="primary">Buy</Button>
-              </Card.Body>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Button className="text-center">Buy</Button>
+                    <Button className="text-center">Sell</Button>
+                  </>) : (
+                  <p className="text-center">Login to buy stocks </p>
+                )}
+            </Card.Body>
             </Card>
           </Col>
-
           <Col>
-          <Card bg={"dark"} text={"white"} className="mb-3">
+            <Card bg={"dark"} text={"white"} className="mb-3">
               <Card.Body>
                 <Card.Title className="text-center">
                   <MDBIcon fab icon="facebook" size="2x" />
@@ -108,16 +119,21 @@ const Home = () => {
                 <Card.Text>
                   <ListGroup>
                     <ListGroup.Item variant="info">Ticker: FB</ListGroup.Item>
-                    <ListGroup.Item variant="success">Price: </ListGroup.Item>
+                    <ListGroup.Item variant="success">Price: 20</ListGroup.Item>
                     <ListGroup.Item action href="/charts" variant="primary">
                       Chart
                     </ListGroup.Item>
                   </ListGroup>
                 </Card.Text>
-                <Button variant="primary">Buy</Button>
-              </Card.Body>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Button className="text-center">Buy</Button>
+                    <Button className="text-center">Sell</Button>
+                  </>) : (
+                  <p className="text-center">Login to buy stocks </p>
+                )}
+            </Card.Body>
             </Card>
-
             <Card bg={"dark"} text={"white"} className="mb-3">
               <Card.Body>
                 <Card.Title className="text-center">
@@ -126,16 +142,21 @@ const Home = () => {
                 <Card.Text>
                   <ListGroup>
                     <ListGroup.Item variant="info">Ticker: TWTR</ListGroup.Item>
-                    <ListGroup.Item variant="success">Price: </ListGroup.Item>
+                    <ListGroup.Item variant="success">Price: 30</ListGroup.Item>
                     <ListGroup.Item action href="/charts" variant="primary">
                       Chart
                     </ListGroup.Item>
                   </ListGroup>
                 </Card.Text>
-                <Button variant="primary">Buy</Button>
-              </Card.Body>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Button className="text-center">Buy</Button>
+                    <Button className="text-center">Sell</Button>
+                  </>) : (
+                  <p className="text-center">Login to buy stocks </p>
+                )}
+            </Card.Body>
             </Card>
-
             <Card bg={"dark"} text={"white"} className="mb-3">
               <Card.Body>
                 <Card.Title className="text-center">
@@ -146,14 +167,19 @@ const Home = () => {
                     <ListGroup.Item variant="info">
                       Ticker: GOOGL
                     </ListGroup.Item>
-                    <ListGroup.Item variant="success">Price: </ListGroup.Item>
+                    <ListGroup.Item variant="success">Price: 50</ListGroup.Item>
                     <ListGroup.Item action href="/charts" variant="primary">
                       Chart
                     </ListGroup.Item>
                   </ListGroup>
                 </Card.Text>
-                <Button variant="primary">Buy</Button>
-              </Card.Body>
+                {Auth.loggedIn() ? (
+                  <>
+                    <Button className="text-center">Buy</Button>
+                  </>) : (
+                  <p className="text-center">Login to buy stocks </p>
+                )}
+            </Card.Body>
             </Card>
           </Col>
         </Row>
@@ -161,6 +187,5 @@ const Home = () => {
     </main>
   );
 };
-
 // Export the footer
 export default Home;
